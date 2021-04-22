88cdbe4f0d2776c028d6a0bd03ba546bad939b9d
Luca1152 <t.luca1152@gmail.com>
2021-04-22 17:06:33 +0300

    Create the GameBoard and Player classes
    Now I've got a basic structure for implementing the future game loop.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!")
+const gameBoard = (() => {
+  const _boardMatrix = new Array(3).fill(new Array(3).fill(0))
+
+  const print = () => console.log(_boardMatrix)
+  return { print }
+})()
+
+const Player = (symbol) => {
+  const getSymbol = () => symbol
+  return { getSymbol }
+}
+
+const playerX = Player("X")
+const playerO = Player("O")
